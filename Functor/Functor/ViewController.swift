14d181fef42dca342f5e88558428fa642c19e060
jamesmin <dev.jamesmin@gmail.com>
2021-05-31 15:32:58 +0900

    add Functor

---
+++ Functor/Functor/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Functor
+//
+//  Created by Jaemin Lee on 2021/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        Functor
+//        Monad
+    }
+
+
+}
+
