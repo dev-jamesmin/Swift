bc0896407361a2f1ff194a599fb60a3154ae88c9
jamesmin <dev.jamesmin@gmail.com>
2021-06-16 13:30:39 +0900

    미션3 슈퍼뷰 상속 중간 저장 서브뷰b 변경 필요.

---
+++ AppClip/AppClip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppClip
+//
+//  Created by Jaemin Lee on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViewControllers/README.md
@@ -0,0 +1,7 @@
+# iOS
+
+Reference
+
+뷰컨트롤러 중첩
+뷰 컨테이너 사용.
+https://minominodomino.github.io/devlog/2019/05/19/ios-ContainerViewController/
